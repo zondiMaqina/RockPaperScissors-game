8a0e918912f51229515d150afd5a651959610236
zondimaqina <zondimaqina0@gmail.com>
2024-03-17 00:07:26 +0200

    add pseudocode

---
+++ main.js
@@ -0,0 +1,31 @@
+// user presses "play game"
+// user enters number of rounds
+// number of rounds shows
+// user chooses presses choice
+// computor shows choice 
+// after each round feedback is needed
+// if player wins one round
+//  player gains one point
+// if player loses one round 
+//  computor gains one point
+// if broth player and computor draw
+//  no points earned for both
+// if rounds finished 
+//  winner is shown
+// if winner is showed 
+//  game is restarted
+//  points are reset
+//  rounds reset
+// user enters number of rounds
+ 
+
+
+// DOM
+// if game is started
+//  every element gets ```box-shadow```
+//  game title changes color
+//  body background-color changes 
+// if point is gained 
+//  background is green
+// if no point gained 
+//  screen is red
